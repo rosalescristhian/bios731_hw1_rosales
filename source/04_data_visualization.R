02cebce317b295a670c0673f1180b632a84108b8
rosalescristhian <cristhian.patricio.rosales.castillo@emory.edu>
2026-01-21 00:10:10 -0500

    adding visualization

---
+++ source/04_data_visualization.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(vars)
+library(gridExtra)
+
+load(here::here("data", "clean_data.Rdata"))
+load(here::here("results", "model_results.RData"))
+
+# 1. Time Series Plot
+p1 <- ggplot(final_ts_data, aes(x = date)) +
+  geom_line(aes(y = gdp_growth, color = "GDP Growth")) +
+  geom_line(aes(y = inflation, color = "Inflation")) +
+  geom_line(aes(y = rate, color = "Fed Funds Rate")) +
+  theme_minimal() +
+  labs(title = "US Macroeconomic Variables (1960-Present)",
+       y = "Percent (Annualized)", x = "", color = NULL) +
+  scale_color_manual(values = c("blue", "red", "black"))
+
+ggsave(here::here("results", "ts_plot.png"), plot = p1, width = 8, height = 5)
+
+# 2. Impulse Response Plot
+# Helper function to extract IRF data
+extract_irf <- function(irf_obj, response_var) {
+  period <- 0:(length(irf_obj$irf$rate[, response_var]) - 1)
+  est <- irf_obj$irf$rate[, response_var]
+  lower <- irf_obj$Lower$rate[, response_var]
+  upper <- irf_obj$Upper$rate[, response_var]
+  data.frame(period, est, lower, upper, response = response_var)
+}
+
+irf_gdp_data <- extract_irf(irf_rate, "gdp_growth")
+irf_inf_data <- extract_irf(irf_rate, "inflation")
+plot_data <- rbind(irf_gdp_data, irf_inf_data)
+
+p2 <- ggplot(plot_data, aes(x = period, y = est)) +
+  geom_line(color = "blue") +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2, fill = "blue") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  facet_wrap(~response, scales = "free_y", ncol = 1) +
+  theme_minimal() +
+  labs(title = "Response to 1% Shock in Fed Funds Rate",
+       y = "Response", x = "Quarters after Shock")
+
+ggsave(here::here("results", "irf_plot.png"), plot = p2, width = 6, height = 6)
